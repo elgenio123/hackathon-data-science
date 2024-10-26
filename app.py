2b0fc260ea16a043e67be8b98971be191259da0e
elgenio <rickentchabet@gmail.com>
2024-10-26 10:48:25 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,136 @@
+from flask import Flask, request, jsonify
+from helpers import concatenate_lists, get_male_first_names, get_female_first_names, get_last_names, get_commune_names, generate_students_names
+
+app = Flask(__name__)
+
+@app.route('/boys', methods=['GET'])
+def get_boys_names():
+    try:
+
+        if not all([]):
+            return jsonify({'error': 'Missing parameters'}), 400
+        
+        url = 'https://top-names.info/names.php?S=M&P=CAM'
+        s = get_male_first_names(url)
+
+        return jsonify({'data': str(s), "length": len(s)})
+
+    except Exception as e:
+        # Handle any errors
+        return jsonify({'error': str(e)}), 400
+
+@app.route('/girls', methods=['GET'])
+def get_girls_names():
+    try:
+        event_flag = request.args.get('event_flag')
+
+        if not all([]):
+            return jsonify({'error': 'Missing parameters'}), 400
+        
+        url = 'https://top-names.info/names.php?S=F&P=CAM'
+        s = get_female_first_names(url)
+
+        return jsonify({'data': str(s), "length": len(s)})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+@app.route('/last-names', methods=['GET'])
+def get_last_name():
+    try:
+        
+        url = 'https://forebears.io/cameroon/surnames'
+        s = get_last_names(url)
+
+        return jsonify({'data': str(s), "length": len(s)})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+@app.route('/commune-names', methods=['GET'])
+def get_commune():
+    try:
+        url = 'https://en.m.wikipedia.org/wiki/Communes_of_Cameroon'
+        s = get_commune_names(url)
+
+        return jsonify({'data': str(s), "length": len(s)})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+    
+@app.route('/generate-last-names', methods=['GET'])
+def generate_last_names():
+    try:
+        size = request.args.get('size')
+        seed = request.args.get('seed')
+
+        if not all([size, seed]):
+            return jsonify({'error': 'Missing parameters'}), 400
+        
+        url = 'https://forebears.io/cameroon/surnames'
+        base_list = get_last_names(url)
+        s = generate_students_names(size=size, base_list=base_list, seed=seed)
+
+        return jsonify({'data': str(s), "length": len(s)})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+    
+@app.route('/generate-first-names', methods=['GET'])
+def generate_male_names():
+    try:
+        size = request.args.get('size')
+        seed = request.args.get('seed')
+        gender = request.args.get('gender')
+
+        if not all([size, seed, gender]):
+            return jsonify({'error': 'Missing parameters'}), 400
+        if gender not in ['M', 'F']:
+            return jsonify({'error': 'gender should be M or F'}), 400
+        
+        if gender == 'M':
+            url = 'https://top-names.info/names.php?S=M&P=CAM'
+            base_list = get_male_first_names(url)
+            s = generate_students_names(size=size, base_list=base_list, seed=seed)
+        elif gender == 'F':
+            url = 'https://top-names.info/names.php?S=F&P=CAM'
+            base_list = get_female_first_names(url)
+            s = generate_students_names(size=size, base_list=base_list, seed=seed)
+
+        return jsonify({'data': str(s), "length": len(s)})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+    
+@app.route('/generate-students', methods=['GET'])
+def generate_students():
+    try:
+        males = request.args.get('males')
+        females = request.args.get('females')
+        seed = request.args.get('seed')
+
+        if not all([males, females, seed]):
+            return jsonify({'error': 'Missing parameters'}), 400
+        
+        url = 'https://top-names.info/names.php?S=M&P=CAM'
+        base_list = get_male_first_names(url)
+        male = generate_students_names(size=males, base_list=base_list, seed=seed)
+
+        url = 'https://top-names.info/names.php?S=F&P=CAM'
+        base_list = get_female_first_names(url)
+        female = generate_students_names(size=males, base_list=base_list, seed=seed)
+
+        url = 'https://forebears.io/cameroon/surnames'
+        base_list = get_last_names(url)
+        if males is not None and females is not None:
+            value = int(males) + int(females)
+            last_names = generate_students_names(size=value, base_list=base_list, seed=seed)
+            overall_list = concatenate_lists(last_names, male, female)
+
+        return jsonify({'data': str(overall_list), "length": len(overall_list)})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ helpers.py
@@ -0,0 +1,152 @@
+import requests
+from bs4 import BeautifulSoup
+import random
+
+def get_male_first_names(url):
+    response = requests.get(url)
+
+    i = 0
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        table = soup.find('table', attrs={'rules': 'none'})
+        if table:
+            list_of_male_firstNames = []
+            
+            for row in table.find_all('tr'):
+                if i not in [0,1, 168]:
+                    cells = row.find_all('td') 
+                    print(cells)
+                    first_name = cells[2].text.strip() 
+                    list_of_male_firstNames.append(first_name)
+                    first_name = cells[6].text.strip() 
+                    list_of_male_firstNames.append(first_name)
+                    first_name = cells[10].text.strip() 
+                    list_of_male_firstNames.append(first_name)
+                if i == 168:
+                    cells = row.find_all('td') 
+                    print(cells)
+                    first_name = cells[2].text.strip() 
+                    list_of_male_firstNames.append(first_name)
+                    first_name = cells[6].text.strip() 
+                    list_of_male_firstNames.append(first_name)
+                i += 1 
+            return list_of_male_firstNames
+        else:
+            return "Table with rules='none' not found."
+    else:
+        return f"Failed to retrieve the page, status code: {response.status_code}"
+
+def get_female_first_names(url):
+    response = requests.get(url)
+
+    i = 0
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        table = soup.find('table', attrs={'rules': 'none'})
+        if table:
+            list_of_female_firstNames = []
+            
+            for row in table.find_all('tr'):
+                if i not in [0,1, 165]:
+                    cells = row.find_all('td') 
+                    print(len(table.find_all('tr')))
+                    first_name = cells[2].text.strip() 
+                    list_of_female_firstNames.append(first_name)
+                    first_name = cells[6].text.strip() 
+                    list_of_female_firstNames.append(first_name)
+                    first_name = cells[10].text.strip() 
+                    list_of_female_firstNames.append(first_name)
+                if i == 165:
+                    cells = row.find_all('td') 
+                    print(cells)
+                    first_name = cells[2].text.strip() 
+                    list_of_female_firstNames.append(first_name)
+                i += 1 
+            return list_of_female_firstNames
+        else:
+            return "Table with rules='none' not found."
+    else:
+        return f"Failed to retrieve the page, status code: {response.status_code}"
+    
+def get_last_names(url):
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+    }
+
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        table = soup.find('table', attrs={'class': 'table forename-table'})
+        if table:
+            list_of_female_surNames = []
+            
+            for row in table.find_all('tr'):
+                cells = row.find_all('td') 
+                print(row)
+                if len(cells)>=2:
+
+                    first_name = cells[1].text.strip()
+                    list_of_female_surNames.append(first_name)
+            return list_of_female_surNames
+        else:
+            return "Table with rules='none' not found."
+    else:
+        return f"Failed to retrieve the page, status code: {response}"
+
+def get_commune_names(url):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+
+        soup = BeautifulSoup(response.content, "html.parser")
+        tables = soup.find_all("table")
+        communes = []
+        for table in tables:
+            for row in table.find_all("tr"):
+                cells = row.find_all(["td"])
+                print(cells)
+                text = cells[0].text.strip()
+                if text:
+                    communes.append(text)
+        communes = [commune for commune in communes if commune not in {"Municipality", "Commune"}]
+        
+        return communes
+    else:
+        return f"Failed to retrieve the page, status code: {response}"
+
+def generate_students_names(size, base_list, seed=None):
+
+    size = int(size)
+
+    if size > len(base_list):
+        return "Size too large"
+    if seed is not None:
+        seed= int(seed)
+        random.seed(seed)
+    
+    selected_names = random.choices(base_list, k=size)
+    
+    return selected_names
+
+def concatenate_lists(list300, list140, list160):
+    result = []
+    
+    len140 = len(list140)
+    len160 = len(list160)
+    
+    for i in range(len(list300)):
+        if i < len140:
+            concatenated = list140[i] +' '+ list300[i]
+        else:
+            index_in_160 = i - len140
+            if index_in_160 < len160:
+                concatenated = list160[index_in_160] +' '+ list300[i]
+            else:
+                concatenated = list300[i]
+                
+        result.append(concatenated)
+    
+    return result
+    
